8cba73bce3784244d0714f1cab192229381acdd1
thevorgx <kh.elallali@gmail.com>
2024-03-25 04:29:16 +0000

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # puppet script to manage ssh configuration, 1- add identity file, 2- disable password authentication
-file_line { 'use_private_key_file':
+file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
 }
-file_line { 'disactivate_password_authentication':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
